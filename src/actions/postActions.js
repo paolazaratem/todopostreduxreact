f9742bf04dc1d33f0e0d88a110818d257b2ce682
paolazaratem <paola.zarate@gmail.com>
2020-04-23 19:35:42 -0500

    add react-redux add redux-thunk add provider to App.js create store create actions, types, reducers, postReducer Move fetch from Posts  to postActions
    Paola Zarate

---
+++ src/actions/postActions.js
@@ -0,0 +1,15 @@
+import { FETCH_POSTS, NEW_POST } from "./types";
+
+export const fetchPosts = () => (dispatch) => {
+  console.log("fetching");
+  return function (dispatch) {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) =>
+        dispatch({
+          type: FETCH_POSTS,
+          payload: posts,
+        })
+      );
+  };
+};
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,21 +1,13 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/postActions";
 
 class Posts extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      posts: [],
-    };
-  }
-
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
+    this.props.fetchPosts();
   }
   render() {
-    const postsItems = this.state.posts.map((post) => (
+    const postsItems = this.props.posts.map((post) => (
       <div key={post.id}>
         <h3>{post.title}</h3>
         <p>{post.body}</p>
@@ -30,4 +22,7 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+const mapStateToProps = (state) => ({
+  posts: state.posts.items,
+});
+export default connect(mapStateToProps, { fetchPosts })(Posts);
